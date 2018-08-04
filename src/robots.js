1a0f44fdf2c1a2b56337534cf8c8d7d72656e88a
consonant <consonant1999@gmail.com>
2018-08-04 15:48:43 +0800

    test

---
+++ src/robots.js
@@ -0,0 +1,34 @@
+export const robots = [
+  {
+		id: 1,
+		name: 'asdas',
+		username: 'BBre',
+		email: 'asdfhu@saerew.wer'
+	},
+	{
+		id: 2,
+		name: 'asd',
+		username: 'BBdsfgdfhre',
+		email: 'asdSfsffhu@saerew.wer'
+	},
+	{
+		id: 3,
+		name: 'dfghh',
+		username: 'AES',
+		email: 'wer@saerew.wer'
+	},
+	{
+		id: 4,
+		name: 'dfh',
+		username: 'zEEf',
+		email: 'asDfweu@saerew.wer'
+	},
+	{
+		id: 5,
+		name: 'sdfg',
+		username: 'ETzg',
+		email: 'asdfhASdfu@saerew.wer'
+	}
+]
+
+export default robots;
